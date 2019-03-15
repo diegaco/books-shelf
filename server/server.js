8c7067b10a34a169193de4d7ff01045d07d4668c
diegaco <diegoacosta211@gmail.com>
2019-03-15 19:39:23 -0300

    Book Models & Schemas, POST & GET routes for books

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config').get(process.env.NODE_ENV);
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +15,50 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+
+// GET
+
+app.get('/api/book/:id', (req, res) => {
+  let id = req.params.id;
+  Book.findById(id, (err, doc) => {
+    if (err) return res.status(400).send({ message: 'Book not founded' })
+    res.status(200).send({ data: doc })
+  })
+});
+
+app.get('/api/books', (req, res) => {
+  let skip = parseInt(req.query.skip),
+      limit = parseInt(req.query.limit),
+      order = req.query.order;
+
+  Book.find().skip(skip).sort({ _id: order }).limit(limit).exec((err, doc) => {
+    if(err) return res.status(400).send({message: 'Books not founded'})
+    res.status(200).send({data: doc})
+  })
+
+  // Book.find((err, doc) => {
+  //   if(err) return res.status(400).send({message: 'Books not founded'})
+  //   res.status(200).send({data: doc})
+  // })
+})
+
+
+// POST
+
+app.post('/api/book', (req, res) => {
+  const book = new Book(req.body);
+
+  book.save((err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send({
+      post: true,
+      bookId: doc._id
+    })
+  })
+})
+
+
 
 app.listen(PORT, () => {
   console.log('Server is running');
