7c8b0219271df905568bab3c9d230bc7a87a35c3
diegaco <diegoacosta211@gmail.com>
2019-05-04 16:24:07 -0300

    Restrict some NavItems when annonymus user

--- client/src/components/Header/SideNavbar/SideNavItems.js
+++ client/src/components/Header/SideNavbar/SideNavItems.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import { connect } from 'react-redux';
 
-const SideNavItems = () => {
+const SideNavItems = ({user}) => {
   const items = [
     {
       type: 'navItem',
@@ -16,42 +17,43 @@ const SideNavItems = () => {
       icon: 'file-text-o',
       text: 'My Profile',
       link: '/user',
-      restricted: false
+      restricted: true
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'Add Admins',
       link: '/user/register',
-      restricted: false
+      restricted: true
     },
     {
       type: 'navItem',
       icon: 'sign-in',
       text: 'Login',
       link: '/login',
-      restricted: false
+      restricted: false,
+      exclude: true
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'My Reviews',
       link: '/user/user-reviews',
-      restricted: false
+      restricted: true
     },
     {
       type: 'navItem',
       icon: 'file-text-o',
       text: 'Add Reviews',
       link: '/user/add',
-      restricted: false
+      restricted: true
     },
     {
       type: 'navItem',
       icon: 'sign-out',
       text: 'Logout',
       link: '/user/logout',
-      restricted: false
+      restricted: true
     }
   ];
 
@@ -64,13 +66,27 @@ const SideNavItems = () => {
     </div>
   );
 
+  const renderItems = () => (
+    user.login ?
+      items.map((item, i) => (
+        user.login.isAuth ?
+          (!item.exclude ? sideNavElement(item, i) : null) :
+          (!item.restricted ? sideNavElement(item, i) : null)
+      )) :
+    null
+  );
+
   return (
-    <div>
-      {items.map((item, i) => (
-        sideNavElement(item, i)
-      ))}
-    </div>
+    <>
+      {renderItems()}
+    </>
   );
 };
 
-export default SideNavItems;
+const mapStateToProps = (state) => {
+  return {
+    user: state.users
+  }
+}
+
+export default connect(mapStateToProps)(SideNavItems);
